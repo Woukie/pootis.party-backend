0c39ed709fe20e677f894f39275a4bd3bdd349c7
Woukie <lucaseldon@live.com>
2024-08-02 13:12:11 +0100

    Removed breadcrumb log

--- middleware/metadata.js
+++ middleware/metadata.js
@@ -1,7 +1,6 @@
 module.exports = function metadata(req, res, next) {
   var breadcrumbs = req.url.split("/");
   breadcrumbs = breadcrumbs.slice(1, -1);
-  console.log(breadcrumbs);
   req.app.locals.breadcrumbs = breadcrumbs;
   next();
 };
